b2ede7c429e2654ec7f00a00ff784795513f51ba
Pedro Vicente Gómez Sánchez <pedro@karumi.com>
2020-05-23 18:24:04 +0200

    First time we get something working

--- DeepPanel.py
+++ DeepPanel.py
@@ -29,6 +29,7 @@ def load_data_set():
 def normalize(input_image, input_mask):
     input_image = tf.cast(input_image, tf.float32) / 255.0
     input_mask -= 1
+    print(input_mask)
     return input_image, input_mask
 
 
@@ -55,11 +56,8 @@ def load_image_test(datapoint):
     return input_image, input_mask
 
 
-def display_sample(display_list):
-    """Show side-by-side an input image,
-    the ground truth and the prediction.
-    """
-    plt.figure(figsize=(18, 18))
+def display(display_list):
+    plt.figure(figsize=(15, 15))
 
     title = ['Input Image', 'True Mask', 'Predicted Mask']
 
@@ -71,34 +69,108 @@ def display_sample(display_list):
     plt.show()
 
 
+def create_mask(pred_mask):
+    pred_mask = tf.argmax(pred_mask, axis=-1)
+    pred_mask = pred_mask[..., tf.newaxis]
+    return pred_mask[0]
+
+
+def show_predictions(dataset, num=1):
+    for image, mask in dataset.take(num):
+        pred_mask = model.predict(image)
+        display([image[0], mask[0], create_mask(pred_mask)])
+
+
+class DisplayCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs=None):
+        print('\nSample Prediction after epoch {}\n'.format(epoch + 1))
+
+
 if __name__ == "__main__":
-    print("Downloading info")
+    print("Loading data")
     raw_dataset = load_data_set()
-    print("Info downloaded")
-    BUFFER_SIZE = 1000
+    train_raw_dataset = raw_dataset['train']
+    test_raw_dataset = raw_dataset['test']
     AUTOTUNE = tf.data.experimental.AUTOTUNE
-    SEED = 42
-    BATCH_SIZE = 32
+    TRAIN_LENGTH = 23
+    BATCH_SIZE = 4
+    BUFFER_SIZE = 10
+    STEPS_PER_EPOCH = TRAIN_LENGTH // BATCH_SIZE
 
-    dataset = {"train": raw_dataset['train'], "val": raw_dataset['test']}
+    print("Transforming data into vectors the model can understand")
+    train = train_raw_dataset.map(load_image_train, AUTOTUNE)
+    test = test_raw_dataset.map(load_image_test)
+    train_dataset = train.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
+    train_dataset = train_dataset.prefetch(buffer_size=AUTOTUNE)
+    test_dataset = test.batch(BATCH_SIZE)
 
-    # -- Train Dataset --#
-    dataset['train'] = dataset['train'].map(load_image_train, num_parallel_calls=AUTOTUNE)
-    dataset['train'] = dataset['train'].shuffle(buffer_size=BUFFER_SIZE, seed=SEED)
-    dataset['train'] = dataset['train'].repeat()
-    dataset['train'] = dataset['train'].batch(BATCH_SIZE)
-    dataset['train'] = dataset['train'].prefetch(buffer_size=AUTOTUNE)
-
-    # -- Validation Dataset --#
-    dataset['val'] = dataset['val'].map(load_image_test)
-    dataset['val'] = dataset['val'].repeat()
-    dataset['val'] = dataset['val'].batch(BATCH_SIZE)
-    dataset['val'] = dataset['val'].prefetch(buffer_size=AUTOTUNE)
-
-    print(dataset['train'])
-    print(dataset['val'])
-
-    for image, mask in dataset['train'].take(1):
+    # Optionally show image and masks:
+    for image, mask in train.take(1):
         sample_image, sample_mask = image, mask
+    display([sample_image, sample_mask])
 
-    display_sample([sample_image[0], sample_mask[0]])
+    print("Creating the model")
+    OUTPUT_CHANNELS = 3
+    base_model = tf.keras.applications.MobileNetV2(input_shape=[128, 128, 3], include_top=False)
+    # Use the activations of these layers
+    layer_names = [
+        'block_1_expand_relu',  # 64x64
+        'block_3_expand_relu',  # 32x32
+        'block_6_expand_relu',  # 16x16
+        'block_13_expand_relu',  # 8x8
+        'block_16_project',  # 4x4
+    ]
+    layers = [base_model.get_layer(name).output for name in layer_names]
+    # Create the feature extraction model
+    down_stack = tf.keras.Model(inputs=base_model.input, outputs=layers)
+    down_stack.trainable = False
+    up_stack = [
+        pix2pix.upsample(512, 3),  # 4x4 -> 8x8
+        pix2pix.upsample(256, 3),  # 8x8 -> 16x16
+        pix2pix.upsample(128, 3),  # 16x16 -> 32x32
+        pix2pix.upsample(64, 3),  # 32x32 -> 64x64
+    ]
+
+
+    def unet_model(output_channels):
+        inputs = tf.keras.layers.Input(shape=[128, 128, 3])
+        x = inputs
+
+        # Downsampling through the model
+        skips = down_stack(x)
+        x = skips[-1]
+        skips = reversed(skips[:-1])
+
+        # Upsampling and establishing the skip connections
+        for up, skip in zip(up_stack, skips):
+            x = up(x)
+            concat = tf.keras.layers.Concatenate()
+            x = concat([x, skip])
+
+        # This is the last layer of the model
+        last = tf.keras.layers.Conv2DTranspose(
+            output_channels, 3, strides=2,
+            padding='same')  # 64x64 -> 128x128
+
+        x = last(x)
+
+        return tf.keras.Model(inputs=inputs, outputs=x)
+
+
+    model = unet_model(OUTPUT_CHANNELS)
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+    tf.keras.utils.plot_model(model, show_shapes=True)
+
+    EPOCHS = 20
+    VAL_SUBSPLITS = 5
+    VALIDATION_STEPS = TRAIN_LENGTH // BATCH_SIZE // VAL_SUBSPLITS
+
+    model_history = model.fit(train_dataset, epochs=EPOCHS,
+                              steps_per_epoch=STEPS_PER_EPOCH,
+                              validation_steps=VALIDATION_STEPS,
+                              validation_data=test_dataset,
+                              callbacks=[DisplayCallback()])
+
+    show_predictions(test_dataset, 1)
